bb22ac03558f3846e93f54986c08c95856bdb046
FMR <fmr@bk.ru>
2021-01-13 16:56:27 +0300

    Дз

--- event-object/keysolo/task.js
+++ event-object/keysolo/task.js
@@ -17,13 +17,13 @@ class Game {
   }
 
   registerEvents() {
-    /*
-      TODO:
-      Написать обработчик события, который откликается
-      на каждый введённый символ.
-      В случае правильного ввода слова вызываем this.success()
-      При неправильном вводе символа - this.fail();
-     */
+    document.addEventListener("keypress", (event) => {
+      let char = event.key.toLowerCase();
+      let charText = this.currentSymbol.textContent;
+      (char == charText)?
+      this.success():
+      this.fail();
+    })
   }
 
   success() {
